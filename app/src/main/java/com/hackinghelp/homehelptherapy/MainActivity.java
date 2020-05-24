101764ed1729147eb5295c951ff2023eb9421019
Yan Vitor <yanvit503@gmail.com>
2020-05-24 02:47:06 -0300

    Configuração tela de login

--- app/src/main/java/com/hackinghelp/homehelptherapy/MainActivity.java
+++ app/src/main/java/com/hackinghelp/homehelptherapy/MainActivity.java
@@ -5,16 +5,45 @@ import android.app.Activity;
 import android.content.Intent;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
+
+    EditText txtEmail, txtSenha;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent i = new Intent(this, Cadastro.class);
-        startActivity(i);
+        txtEmail = findViewById(R.id.text_login_email);
+        txtSenha = findViewById(R.id.text_login_senha);
 
     }
+
+
+
+    public void btnLogin(View v)
+    {
+        //Futuramente conectar-se ao WebService e realizar de fato a autenticação
+
+        if(!txtEmail.getText().toString().isEmpty() && !txtSenha.getText().toString().isEmpty())
+        {
+            
+        }
+        else
+        {
+            Toast.makeText(this, "Preencha os campos", Toast.LENGTH_SHORT).show();
+        }
+
+
+    }
+
+    public void btnCadastro(View v)
+    {
+        startActivity(new Intent(this,Cadastro.class));
+    }
+
 }
